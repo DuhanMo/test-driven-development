cdc707644d88e29b09ab094beccfb2050fa04220
Duhan Mo <devduhanmo@gmail.com>
2023-02-09 00:58:56 +0900

    CH08 test: make object
    1. 동일한 메서드(times)의 반환타입을 Money로 통일시켜 중복제거에 가까워짐
    2. 메서드 선언부를 공통 상위클래스로 옮김
    3. 팩터리 메서드를 도입해 테스트 코드에서 콘트리트 하위 클래의 존재 사실을 분리
    4. 하위 클래스가 사라지면 몇몇 테스트는 불필요한 여분이 되는 것임을 자각

--- src/main/java/org/example/tdd/section1/Money.java
+++ src/main/java/org/example/tdd/section1/Money.java
@@ -1,7 +1,17 @@
 package org.example.tdd.section1;
 
-public class Money {
+abstract class Money {
     protected int amount;
+
+    abstract Money times(int multiplier);
+
+    static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount);
+    }
 
     @Override
     public boolean equals(Object obj) {
