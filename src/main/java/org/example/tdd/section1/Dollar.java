c20b0416577520c5d12dad5a6a8e6240aa156094
Duhan Mo <devduhanmo@gmail.com>
2023-02-12 13:09:55 +0900

    CH11 test: the root of all evil
    1. 쓸모가 없어진 하위 클래스를 삭제
    2. 새로운 구조에서는 필요 없게 된 테스트를 제거

--- src/main/java/org/example/tdd/section1/Dollar.java
+++
@@ -1,7 +0,0 @@
-package org.example.tdd.section1;
-
-public class Dollar extends Money {
-    Dollar(int amount, String currency) {
-        super(amount, currency);
-    }
-}
--- src/test/java/org/example/tdd/section1/MoneyTest.java
+++ src/test/java/org/example/tdd/section1/MoneyTest.java
@@ -17,8 +17,6 @@ class MoneyTest {
     public void testEquality() {
         assertEquals(Money.dollar(5), Money.dollar(5));
         assertNotEquals(Money.dollar(5), Money.dollar(6));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
@@ -34,12 +32,5 @@ class MoneyTest {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
 
-    }
-
-    @Test
-    public void testDifferentClassEquality() {
-        assertTrue(new Money(10, "CHF").equals(
-                new Franc(10, "CHF")
-        ));
     }
 }
